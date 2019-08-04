6ede3b160150422367e208ddbbbf672ad99f4c98
Plant Lady <codeplantsbylouie@gmail.com>
2019-08-04 15:00:23 -0400

    functions created; bugs though

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function) {
+$(document).ready(function() {
   let animals = [turtle, skunk, capybara, puppy];
 
   // FUNCTION TO MAKE BUTTONS AND ADD TO PAGE
@@ -14,10 +14,70 @@ $(document).ready(function) {
     }
   }
 
+  $(document).on("click", ".gif-button", () => {
+    $("#gifs").empty();
+    $(".gif-button").removeClass("active");
+    $(this).addClass("active");
 
+    let type = $(this).attr("data-type");
+    let queryURL =
+      "http://api.giphy.com/v1/gifs/search?q=" +
+      type +
+      "&api_key=dc6zaTOxFJmzC&limit=10";
 
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(response => {
+      let results = response.data;
 
+      for (var i = 0; i < results.length; i++) {
+        let gifDiv = $('<div class="gif-item">');
 
+        let rating = results[i].rating;
 
+        let p = $("<p>").text("Rating: " + rating);
 
-}
+        let animated = results[i].images.fixed_height.url;
+        let still = results[i].images.fixed_height_still.url;
+
+        let gifImage = $("<img>");
+
+        gifImage.attr("src", still);
+        gifImage.attr("data-still", still);
+        gifImage.attr("data-animate", animated);
+        gifImage.attr("data-state", "still");
+        gifImage.addClass("gif-image");
+
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+      }
+    });
+  });
+
+  $(document).on("click", ".gif-image", () => {
+    let state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+  $("#add-gif").on("click", event => {
+    event.preventDefault();
+    let newGif = $("input")
+      .eq(0)
+      .val();
+
+    if (newGif.length > 2) {
+      gifs.push(newGif);
+    }
+    populateButtons(gifs, "gif-button", "#gif-buttons");
+  });
+
+  populateButtons(gifs, "gif-button", "#gif-buttons");
+});
