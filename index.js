6657c3a2a46b9e0462ae9d91b093bff2001fbd96
Plant Lady <codeplantsbylouie@gmail.com>
2019-08-04 14:33:26 -0400

    create a function to maket he new buttons for new gif topics

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 $(document).ready(function) {
-  
+  let animals = [turtle, skunk, capybara, puppy];
+
+  // FUNCTION TO MAKE BUTTONS AND ADD TO PAGE
+  function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
+
+    for (var i = 0; i < arrayToUse.length; i++) {
+      let a = $("<button>");
+      a.addClass(classToAdd);
+      a.attr("data-type", arrayToUse[i]);
+      a.text(arrayToUse[i]);
+      $(areaToAddTo).append(a);
+    }
+  }
+
+
+
+
+
+
+
 }
